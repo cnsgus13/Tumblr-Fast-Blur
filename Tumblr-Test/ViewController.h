a0827846452944fc72b795eaff9539948d654da3
yiqin <yiqin.mems@gmail.com>
2014-03-09 16:19:38 -0400

    Initial Commit

---
+++ Tumblr-Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tumblr-Test
+//
+//  Created by yiqin on 3/9/14.
+//  Copyright (c) 2014 purdue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Tumblr-Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tumblr-Test
+//
+//  Created by yiqin on 3/9/14.
+//  Copyright (c) 2014 purdue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
